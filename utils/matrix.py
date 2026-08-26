630964fe1e929a9940e4ccba2c813df9447f9a23
travletothefurureprogramming <travletothefurureprogramming@gmail.com>
2026-08-26 18:32:43 +0300

    Initialize matrix display orientation by rotating it 180 degrees

--- utils/matrix.py
+++ utils/matrix.py
@@ -1,7 +1,9 @@
 import scrollphathd as sphd
 import asyncio
+from services.weather import get_temperature
 
 current_text = "READY"
+sphd.rotate(180)
 
 def set_text(text: str):
     global current_text
@@ -11,6 +13,11 @@ def set_text(text: str):
 
 async def start_matrix_loop():
     set_text("RoomOps Zero")
+    for i in range(3):
+        sphd.show()
+        sphd.scroll(1)
+        await asyncio.sleep(0.05)
+    set_text(f"{await get_temperature("Kavala")}C")
     while True:
         sphd.show()
         sphd.scroll(1)
